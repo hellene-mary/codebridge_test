29b30f6bb581b13ae1412d381619768b5a7c468e
Grek Maria <grekmariaa@gmail.com>
2023-01-18 17:19:05 +0200

    Update filterBykeywords.js

--- src/components/utils/filterBykeywords.js
+++ src/components/utils/filterBykeywords.js
@@ -1,26 +1,41 @@
+function checkArrFilter(filteredArray) {
+  console.log("filteredArray in check", filteredArray)
+
+  for (const i of filteredArray) {
+    console.log("i", i.id)
+  }
+}
+
 export function filterByKeyword(search, articles) {
+  const arrSearch = search.split(" ")
+
   const filteredArray = []
 
   for (const article of articles) {
-    if (article.title.toLowerCase().indexOf(search.toLowerCase()) !== -1) {
-      filteredArray.push({ ...article, priority: 1 })
-    } else if (
-      article.summary.toLowerCase().indexOf(search.toLowerCase()) !== -1
-    ) {
-      filteredArray.push({ ...article, priority: 2 })
+    for (const search of arrSearch) {
+      if (article.title.toLowerCase().indexOf(search.toLowerCase()) !== -1) {
+        filteredArray.push({ ...article, priority: 1 })
+      } else if (
+        article.summary.toLowerCase().indexOf(search.toLowerCase()) !== -1
+      ) {
+        filteredArray.push({ ...article, priority: 2 })
+      }
     }
   }
 
-  // console.log("filteredArray", filteredArray)
+  checkArrFilter(filteredArray)
+
+  // console.log("filteredArray in filter", filteredArray)
+
   return filteredArray
 }
 
-// const filterTitleArticles = articles.filter((article) =>
-//   article.title.toLowerCase().includes(search.toLowerCase())
-// )
-// console.log("filterTitleArticle", filterTitleArticles)
-
-// const filterSummaryArticles = articles.filter((article) =>
-//   article.summary.toLowerCase().includes(search.toLowerCase())
-// )
-// console.log("filterSummaryArticles", filterSummaryArticles)
+// for (const article of articles) {
+//   if (article.title.toLowerCase().indexOf(search.toLowerCase()) !== -1) {
+//     filteredArray.push({ ...article, priority: 1 })
+//   } else if (
+//     article.summary.toLowerCase().indexOf(search.toLowerCase()) !== -1
+//   ) {
+//     filteredArray.push({ ...article, priority: 2 })
+//   }
+// }
